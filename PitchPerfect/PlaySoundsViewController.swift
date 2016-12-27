56a34f7b148244b52c827b8a1df238a3a98259c9
Bill Heitzeg <william.heitzeg@tdameritrade.com>
2016-12-27 14:18:10 -0500

    Removed extraneous comment

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -69,15 +69,4 @@ class PlaySoundsViewController: UIViewController {
         configureUI(.notPlaying)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
